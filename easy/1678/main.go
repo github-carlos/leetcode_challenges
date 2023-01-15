24aed01890658cf631e14a2a7f1e6f5295bfcbf9
github-carlos <caeduob@gmail.com>
2023-01-15 01:11:08 -0300

    saving problema 412

--- easy/1678/main.go
+++ easy/1678/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func maximumWealth(accounts [][]int) int {
-	
+
 	var maxValue = 0
 
 	for _, accounts := range accounts {
@@ -20,9 +20,9 @@ func maximumWealth(accounts [][]int) int {
 }
 
 func main() {
-	test1 := [][]int{{1, 2, 3}, {3, 2, 1}} // would be 6
+	// test1 := [][]int{{1, 2, 3}, {3, 2, 1}} // would be 6
 	test2 := [][]int{{1, 5}, {7, 3}, {3, 5}} // would be 10
-	fmt.Println(maximumWealth(test1))
+	// fmt.Println(maximumWealth(test1))
 	fmt.Println(maximumWealth(test2))
 	// maximumWealth(test2)
 }
---
+++ easy/412/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func fizzBuzz(n int) []string {
+	
+	answer := []string{}
+
+	for i := 1; i <= n; i++ {
+		if i == 1 || i == 2 {
+			answer = append(answer, strconv.Itoa(i))
+			continue
+		}
+
+		if (i % 3 == 0 && i % 5 == 0) {
+			answer = append(answer, "FizzBuzz")
+			continue
+		}
+
+		if (i % 3 == 0) {
+			answer = append(answer, "Fizz")
+			continue
+		}
+
+		if (i % 5 == 0) {
+			answer = append(answer, "Buzz")
+			continue
+		}
+
+		answer = append(answer, strconv.Itoa(i))
+	}
+
+	return answer 
+}
+
+func main() {
+	fmt.Println(fizzBuzz(3)) // shouuld be ["1", "2", "Fizz"]
+}
